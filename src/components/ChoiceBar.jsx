e61bf01dbbc11cc2a2872756996f727c58230712
Muhammad Junaid Zubair Malik <junaidzm@connect.hku.hk>
2023-05-13 14:48:43 +0800

    add post request in ChoiceBar.jsx

--- src/components/ChoiceBar.jsx
+++ src/components/ChoiceBar.jsx
@@ -4,9 +4,16 @@ export function ChoiceBar(props) {
   //const { choice, onclick } = props
   const { question_index, choice_index, choice, incrementQuestionNumber} = props
  
-  const onclick = () => {
+  const onclick = async () => {
     incrementQuestionNumber()
 
+    const response = await fetch("http://127.0.0.1:8000/bias_test/api/submit-choice/", {
+        method: "POST",
+        header: {"Content-Type": "application/json"},
+        body: JSON.stringify({question_index, choice_index})
+    });
+    const json = await response.json();
+    console.log(json);
     alert('Choice Saved. ' + question_index.toString()+': '+choice_index.toString())
   }
   return (
